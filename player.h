c2274f20ec0173e6771c34e8c60b62e605fa7fe6
Zhao Kexin <zhao0725@connect.hku.hk>
2024-11-26 23:18:32 +0800

    add the name member

--- player.h
+++ player.h
@@ -7,11 +7,12 @@
 
 struct Player {
     int id;                        // Unique identifier for the player
+    char name[50];                 // Name of the player
     struct Card hand[MAX_CARDS];   // Cards currently held by the player
     struct Card played_cards[MAX_CARDS]; // Cards played during the turn
     int num_played_cards;          // Number of cards played during the turn
     bool is_eliminated;            // Flag to indicate if the player is eliminated
-    int death_chamber;             // Chamber number in the death roulette
+    int death_chamber;             // Chamber number in the death roulette (1~6)
 };
 
 #endif
