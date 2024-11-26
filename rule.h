72b84ec8366dab878107e31fbdba98540b26bc7e
Zhao Kexin <zhao0725@connect.hku.hk>
2024-11-26 23:20:38 +0800

    head file of rule

---
+++ rule.h
@@ -0,0 +1,34 @@
+#ifndef RULE_H
+#define RULE_H
+
+#include <stdbool.h> // For 'bool' type
+#include "player.h"  // Include Player structure
+#include "card.h"    // Include Card structure
+
+/**
+ * Checks if the cards played by the player match their declaration.
+ * 
+ * played_cards: Array of cards played by the current player.
+ * num_cards: Number of cards played in the current turn.
+ * called_value: The declared card value for this round.
+ * 
+ * Return True if the player is honest. Return False if the player is bluffing.
+ */
+bool check_bluff(struct Card* played_cards, int num_cards, int called_value);
+
+/**
+ * Handles the result of a bluff challenge.
+ * 
+ * challenger Pointer to the Player who initiated the challenge.
+ * current_player Pointer to the Player whose play is being challenged.
+ */
+void handle_challenge(struct Player* challenger, struct Player* current_player);
+
+/**
+ * Executes the death roulette for a player.
+ * 
+ * player Pointer to the Player undergoing the death roulette.
+ */
+void trigger_death_roulette(struct Player* player);
+
+#endif
