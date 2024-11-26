a6ea2d339cd51af7f19b7d7823a412e761205278
Zhao Kexin <zhao0725@connect.hku.hk>
2024-11-26 23:00:15 +0800

    the head file for the rules. include the functions of: check_bluff, handle_challenge, trigger_death_roulette

---
+++ player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "card.h" // Include the Card struct definition
+
+#define MAX_CARDS 5 // Maximum number of cards a player can hold
+
+struct Player {
+    int id;                        // Unique identifier for the player
+    struct Card hand[MAX_CARDS];   // Cards currently held by the player
+    struct Card played_cards[MAX_CARDS]; // Cards played during the turn
+    int num_played_cards;          // Number of cards played during the turn
+    bool is_eliminated;            // Flag to indicate if the player is eliminated
+};
+
+#endif
