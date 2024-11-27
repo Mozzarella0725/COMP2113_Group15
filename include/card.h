2f76d17f800576df2bda0d334e422661d3739def
Zhang Muqun <zhangmq1@connect.hku.hk>
2024-11-28 05:29:32 +0800

    Update card.h

--- include/card.h
+++ include/card.h
@@ -1,21 +1,21 @@
 #ifndef CARD_H
 #define CARD_H
 
-// Card structure
+// Card structure: represents a card, including its value and suit
 typedef struct {
-    int value;      // Card value (1-13)
+    int value;      // Card value (1-13, corresponding to Aces, Kings, Queens)
     char suit;      // Card suit (H: Hearts, S: Spades, C: Clubs, D: Diamonds)
 } Card;
 
-// Deck structure
+// Deck structure: represents the entire deck of cards
 typedef struct {
-    Card deck[104]; // Support up to two decks
-    int size;       // Total size of the deck
-    int top;        // Index for the top of the deck
+    Card deck[20];  // Deck of a maximum of 20 cards
+    int size;       // Current size of the deck
+    int top;        // Index pointing to the top of the deck
 } Deck;
 
-// Function declarations
-void generate_deck(Deck* deck, int num_decks);
+// Function declarations for card management
+void generate_deck(Deck* deck);
 void shuffle_deck(Deck* deck);
 void deal_cards(Deck* deck, Card* hand, int num_cards);
 void print_deck(Deck* deck);
