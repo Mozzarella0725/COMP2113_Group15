7731bc9eaaf2cd22fa313184892fcb2bfb6934a6
tky0503 <u3600184@connect.hku.hk>
2024-11-28 17:13:47 +0800

    Update action_key.cpp

--- action_key.cpp
+++ action_key.cpp
@@ -1,1 +1,13 @@
-#include action_key.h
+#include "action_key.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void display_player_hand() {
+}
+void quit_game() {
+  cout << "You have choose to quit this game!" << endl;
+  cout << "Thanks for playing!" << endl;
+  exit(0);
+}
