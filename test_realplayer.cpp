638efe3e9cfcd50e277b061cb3f7b490fbcb1d06
tky0503 <u3600184@connect.hku.hk>
2024-11-28 17:03:43 +0800

    Update test_realplayer.cpp

--- test_realplayer.cpp
+++ test_realplayer.cpp
@@ -1,19 +1,24 @@
 #include realplayer.h
+#include action_key.h
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-  int command;
+  string command;
   cout << "Now it's your turn, you can choose:" << endl;
-  cout << "1. Play cards." << "\n" << "2. Skip this turn." << endl;
-  cout << "Please enter your choice: " << "\n" << "(enter corresponding number: 1 or 2)" << endl;
+  cout << "1. Play cards." << "\n" << "2. Skip this turn." << "\n" << "Quit Game" << endl;
+  cout << "Please enter your choice: " << "\n" << "(enter corresponding number: 1, 2, or quit)" << endl;
   cin >> command;
-  while (command != 2) {
-      handle_player_input();
-    }
-    cout << "Enter your next choice: " << endl;
-    cin >> command;
+  if (command == "1") {
+    display_player_hand();
+    handle_player_input();
+  }else if (command == "2") {
+    cout << "You have skipped your turn." << endl;
+  }else if (command == "quit") {
+    quit_game();
+  }else {
+    cout << "Invalid choice. Please enter 1, 2, or quit" << endl;
   }
-  cout << "You have skipped your turn." << endl;
   return 0;
 }
