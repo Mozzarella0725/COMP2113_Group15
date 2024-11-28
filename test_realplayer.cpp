31c62969405ffc56821469d045ddb8e2a2b643e3
tky0503 <u3600184@connect.hku.hk>
2024-11-28 16:36:54 +0800

    Update test_realplayer.cpp

--- test_realplayer.cpp
+++ test_realplayer.cpp
@@ -5,13 +5,10 @@ using namespace std;
 int main() {
   int command;
   cout << "Now it's your turn, you can choose:" << endl;
-  cout << "1. Display your card." << "\n" << "2. Play cards." << "\n" << "3. Skip this turn." << endl;
-  cout << "Please enter your choice: " << "\n" << "(enter corresponding number: 1, 2, or 3.)" << endl;
+  cout << "1. Play cards." << "\n" << "2. Skip this turn." << endl;
+  cout << "Please enter your choice: " << "\n" << "(enter corresponding number: 1 or 2)" << endl;
   cin >> command;
-  while (command != 3) {
-    if (command == 1) {
-      display_player_hand();
-    }else {
+  while (command != 2) {
       handle_player_input();
     }
     cout << "Enter your next choice: " << endl;
