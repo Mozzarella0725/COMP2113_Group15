2e024255852c6d3f2ee07515e425b6acb29d427c
tky0503 <u3600184@connect.hku.hk>
2024-11-28 17:39:10 +0800

    Update action_key.cpp

--- action_key.cpp
+++ action_key.cpp
@@ -6,8 +6,25 @@ using namespace std;
 
 void display_player_hand() {
 }
-void quit_game() {
-  cout << "You have choose to quit this game!" << endl;
-  cout << "Thanks for playing!" << endl;
-  exit(0);
+
+bool quit_game() {
+  string check;
+  cout << "Are you sure you want to quit the game?" << endl;
+  cout << "After quitting the game, the records of this game will be cleared and the next game will be a new one." << endl;
+  cout << "Please enter 'Yes' or 'No': ";
+  cin >> check;
+
+  while (check != "Yes" && check != "No" && check != "yes" && check != "no") {
+    cout << "Invalid choice. Please enter 'Yes' or 'No': ";
+    cin >> check;
+  }
+
+  if (check == "Yes" || check == "yes") {
+    cout << "You have choose to quit this game!" << endl;
+    cout << "Thanks for playing!" << endl;
+    return true;
+  } else {
+    cout << "You chose not to quit the game." << endl;
+    return false;
+  }
 }
