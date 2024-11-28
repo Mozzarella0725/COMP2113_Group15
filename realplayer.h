60198b121a703b9f621b4f985152be2bcb7da308
tky0503 <u3600184@connect.hku.hk>
2024-11-28 18:45:50 +0800

    Update realplayer.h

--- realplayer.h
+++ realplayer.h
@@ -1,12 +1,9 @@
 #ifndef REALPLAYER_H
 #define REALPLAYER_H
 
-#include <iostream>
-#include <string>
 #include "card.h"
 
-void display_player_hand() const；
-void handle_player_input()；
-bool quit_game();
+void display_player_hand(Card* player_hand, int num_player_cards);
+void handle_player_input(Card* player_hand, int& num_player_cards);
 
 #endif
