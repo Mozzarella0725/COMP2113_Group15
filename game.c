7d2b6e4c115abc3d1318e6fc75b7822209d709ac
Zhao Kexin <zhao0725@connect.hku.hk>
2024-12-01 17:22:14 +0800

    delete the unneeded member

--- game.c
+++ game.c
@@ -24,7 +24,7 @@ void handle_turn(Player* player, Deck* deck) {
     printf("Player %d's turn\n", player->id);
     printf("Hand: ");
     for (int i = 0; i < player->hand_count; i++) {
-        printf("%d%c ", player->hand[i].value, player->hand[i].suit);
+        printf("%d%c ", player->hand[i].value);
     }
     printf("\n");
 
