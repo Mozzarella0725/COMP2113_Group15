eb2dc183955931c73201bec72d95d99181cfd121
zakkzhou <130914718+zakkzhou@users.noreply.github.com>
2024-12-05 03:20:49 +0800

    Create game_state.h

---
+++ game_state.h
@@ -0,0 +1,32 @@
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
+
+#include <vector>
+#include <string>
+#include "card.h"
+#include "game.h"
+#include "rule.h"
+#include "realplayer.h"
+#include "AI_Player.cpp"
+
+#define MAX_PLAYERS 4
+#define MAX_CARDS 5
+
+// Structure to hold player information
+struct GamePlayer {
+    std::string name;
+    bool is_ai;
+    Player* human;
+    AIPlayer* ai;
+    std::vector<Card> played_cards;
+    int death_chamber;
+
+    // Constructor
+    GamePlayer() : is_ai(false), human(nullptr), ai(nullptr), death_chamber(1) {}
+};
+
+void start_new_round(std::vector<GamePlayer>& players);
+void print_game_state(std::vector<GamePlayer>& players);
+void handle_challenge(std::vector<GamePlayer>& players, int challenger, int challenged);
+
+#endif 
