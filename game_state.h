99d2093b8e0ce6a2bba4cf40493f1e87711ad89c
zakkzhou <130914718+zakkzhou@users.noreply.github.com>
2024-12-06 21:07:14 +0800

    Update game_state.h

--- game_state.h
+++ game_state.h
@@ -4,31 +4,24 @@
 #include <vector>
 #include <string>
 #include "card.h"
-#include "game.h"
-#include "rule.h"
 #include "realplayer.h"
 
-#define MAX_PLAYERS 4
-#define MAX_CARDS 5
-
-// Forward declare AIPlayer
 class AIPlayer;
 
-// Structure to hold player information
 struct GamePlayer {
-    std::string name;
     bool is_ai;
-    Player* human;
-    AIPlayer* ai;
-    std::vector<Card> played_cards;
+    std::string name;
+    union {
+        AIPlayer* ai;
+        Player* human;
+    };
     int death_chamber;
-
-    // Constructor
-    GamePlayer() : is_ai(false), human(nullptr), ai(nullptr), death_chamber(1) {}
+    std::vector<Card> played_cards;
 };
 
 void start_new_round(std::vector<GamePlayer>& players);
 void print_game_state(std::vector<GamePlayer>& players);
-void handle_challenge(std::vector<GamePlayer>& players, int challenger, int challenged);
 
-#endif 
+extern int called_value;
+
+#endif
